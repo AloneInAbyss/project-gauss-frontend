29f3053a513400d43193cc9f261d4f0a5e7aa0b7
Thiago Assi <thiago-assi@outlook.com>
2023-06-04 19:42:59 -0300

    refactoring card component for reusability

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HomeCard } from 'src/app/models/cards/home-card';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: []
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  cards: HomeCard[];
+
+  constructor(private router: Router) {
+    this.cards = [
+      {
+        subtitle: 'Guide',
+        title: 'Honkai Star Rail',
+        content:
+          'Tools for calculations on artifacts stats, level up materials, and more!',
+        route: '/star-rail',
+        button: 'GO'
+      },
+      {
+        subtitle: 'Guide',
+        title: 'Genshin Impact',
+        content:
+          'Tools for calculations on artifacts stats, level up materials, and more!',
+        route: '/',
+        button: 'GO'
+      }
+    ];
+  }
 
   navigateToPage(pageName: string) {
     this.router.navigate([pageName]);
