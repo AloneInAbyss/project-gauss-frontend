f96a93e3014fdb7738896918fa058e53fd045556
Thiago Assi <thiago-assi@outlook.com>
2023-06-05 19:59:53 -0300

    add navigation card to star rail section

--- src/app/pages/star-rail/star-rail.component.ts
+++ src/app/pages/star-rail/star-rail.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { StarRailRoute } from 'src/app/routes/routes';
+import { NavigationCard } from 'src/app/shared/components/navigation-cards/models/navigation-card.model';
 
 @Component({
   selector: 'app-star-rail',
   templateUrl: './star-rail.component.html',
   styleUrls: []
 })
-export class StarRailComponent {}
+export class StarRailComponent {
+  cards: NavigationCard[] = [
+    {
+      subtitle: 'Info',
+      title: 'Relics',
+      content: 'View each relic type, corresponding main stats and possible substats values',
+      route: '/' + StarRailRoute.path,
+      button: 'GO'
+    }
+  ];
+}
--- src/app/pages/star-rail/star-rail.module.ts
+++ src/app/pages/star-rail/star-rail.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
 
 import { StarRailComponent } from './star-rail.component';
 
 @NgModule({
   declarations: [StarRailComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, SharedComponentsModule]
 })
 export class StarRailModule {}
