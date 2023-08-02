807b8ba6a5812f0d87cff011531cf068cefdcca7
Thiago Assi <thiago-assi@outlook.com>
2023-08-02 20:00:36 -0300

    test: dialog parameters

--- src/app/shared/components/custom-dialog/custom-dialog.component.spec.ts
+++ src/app/shared/components/custom-dialog/custom-dialog.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CustomDialogComponent } from './custom-dialog.component';
 import { CustomDialogData } from './models/custom-dialog-data.model';
+import { getTranslocoModule } from '../../modules/transloco-testing.module';
+import { TranslocoService } from '@ngneat/transloco';
 
 const dialogData: CustomDialogData = {
   buttonLabel: 'Button Label',
@@ -16,6 +18,7 @@ describe('CustomDialogComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [getTranslocoModule()],
       declarations: [CustomDialogComponent],
       providers: [
         {
@@ -29,7 +32,65 @@ describe('CustomDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create with title, description and button label', () => {
+    const nativeElement = fixture.nativeElement as HTMLElement;
+
+    const heading = nativeElement.querySelector(
+      'div > h1'
+    ) as HTMLHeadingElement;
+    const paragraph = nativeElement.querySelector(
+      'div > div > p'
+    ) as HTMLParagraphElement;
+    const button = nativeElement.querySelector(
+      'div > div > div > button'
+    ) as HTMLButtonElement;
+
+    expect(heading.textContent).toBe(dialogData.title);
+    expect(paragraph.textContent).toBe(dialogData.description);
+    expect(button.textContent).toBe(dialogData.buttonLabel);
+  });
+});
+
+describe('CustomDialogComponent without MAT_DIALOG_DATA', () => {
+  let component: CustomDialogComponent;
+  let fixture: ComponentFixture<CustomDialogComponent>;
+  let t: TranslocoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [getTranslocoModule()],
+      declarations: [CustomDialogComponent],
+      providers: [
+        TranslocoService,
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        }
+      ]
+    });
+
+    t = TestBed.inject(TranslocoService);
+
+    fixture = TestBed.createComponent(CustomDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create with title, description and button label', () => {
+    const nativeElement = fixture.nativeElement as HTMLElement;
+
+    const heading = nativeElement.querySelector(
+      'div > h1'
+    ) as HTMLHeadingElement;
+    const paragraph = nativeElement.querySelector(
+      'div > div > p'
+    ) as HTMLParagraphElement;
+    const button = nativeElement.querySelector(
+      'div > div > div > button'
+    ) as HTMLButtonElement;
+
+    expect(heading.textContent).toBe('');
+    expect(paragraph.textContent).toBe('');
+    expect(button.textContent).toBe(t.translate('button.close'));
   });
 });
